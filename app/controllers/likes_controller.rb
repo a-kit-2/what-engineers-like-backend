95b51d7a62815a6191efb7ec2183b0815b48021a
a-kit-2 <akito.nakamura.c@gmail.com>
2021-07-26 10:00:51 +0900

    controller制作

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,35 @@
+class LikesController < ApplicationController
+  def create
+    @already_like = Like.find_by(name: likes_params[:name])
+    if @already_like
+      @already_like.count += 1
+      if @already_like.save
+        render json: {add_count: true}
+      else
+        render json: {add_count: false}
+      end
+    else
+      @new_like = Like.new(likes_params)
+      if @new_like.save
+        render json: {create_like: true}
+      else
+        render json: {create_like: false}
+      end
+    end
+  end
+
+  def show
+    @likes = Like.all
+    if @likes
+      render json: {show_likes: true, likes: @likes}
+    else
+      render json: {show_likes: false}
+    end
+  end
+
+  private
+
+  def likes_params
+      params.require(:like).permit(:name, :count)
+  end 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post '/new_likes', to: 'likes#create'
+  get '/show_likes', to: 'likes#show'
 end

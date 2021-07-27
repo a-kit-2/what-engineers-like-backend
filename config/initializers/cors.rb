489ba1ec6a20bbc75c315db8277a7431f0762c80
a-kit-2 <akito.nakamura.c@gmail.com>
2021-07-27 16:24:28 +0900

    cors設定

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -15,13 +15,13 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       credentials: true
   end
 
-  #本番環境用のオリジン設定
-  # allow do
-  #   origins ENV['FRONTEND_URL'] # フロントエンド側のURL
+  # 本番環境用のオリジン設定
+  allow do
+    origins ENV['FRONTEND_URL'] # フロントエンド側のURL
 
-  #   resource '*',
-  #     headers: :any,
-  #     methods: [:get, :post, :put, :patch, :delete, :options, :head],
-  #     credentials: true
-  # end
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
+  end
 end

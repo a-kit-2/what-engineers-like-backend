469549fd26ed5cade0ebeebd3b8b6ec6b88d22b8
a-kit-2 <akito.nakamura.c@gmail.com>
2021-07-28 11:02:08 +0900

    production用に修正

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -15,13 +15,15 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       credentials: true
   end
 
-  # 本番環境用のオリジン設定
-  allow do
-    origins ENV['FRONTEND_URL'] # フロントエンド側のURL
+  if Rails.env.production?
+    # 本番環境用のオリジン設定
+    allow do
+      origins ENV['FRONTEND_URL'] # フロントエンド側のURL
 
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
+      resource '*',
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head],
+        credentials: true
+    end
   end
 end
